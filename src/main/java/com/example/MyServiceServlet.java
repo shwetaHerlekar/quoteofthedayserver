fc2a2ca78d507c10cdb24b003afeb3f13b33db3a
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-08-23 17:15:23 +0530

    webhook

---
+++ src/main/java/com/example/MyServiceServlet.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.io.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ai.api.AIServiceException;
+import ai.api.model.AIResponse;
+import ai.api.web.AIServiceServlet;
+
+
+
+// [START example]
+@SuppressWarnings("serial")
+public class MyServiceServlet extends AIServiceServlet {
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    
+	
+	String sessionId = req.getParameter("sessionId");
+	try{
+	
+		AIResponse aiResponse = request(req.getParameter("query"), sessionId);
+		resp.setContentType("text/plain");
+		resp.getWriter().append(aiResponse.getResult().getFulfillment().getSpeech());
+	}
+	
+	catch(AIServiceException e){
+	
+	}
+  }
+}

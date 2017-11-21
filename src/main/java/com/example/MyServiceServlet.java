5cd95ff87c5ed7e15fabc6dcf4d1548f533a2b48
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-11-21 08:58:16 +0530

    test

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -1,13 +1,21 @@
 package com.example;
 
 import java.io.*;
-
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 
 // [START example]
@@ -20,7 +28,11 @@ public class MyServiceServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		log.info("inside get method");
-		resp.getWriter().write("Inside Get");
-		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Filter p = new FilterPredicate("EmpID", FilterOperator.EQUAL, "18831");
+		Query q = new Query("PersistentEmployees").setFilter(p);
+		PreparedQuery pq = datastore.prepare(q);
+		Entity result = pq.asSingleEntity();
+		log.info(result.toString());
 	}
 }

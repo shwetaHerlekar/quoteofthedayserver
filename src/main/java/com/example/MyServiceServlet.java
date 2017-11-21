ef1b97bd8215d3d928eb8202b224764d3ce4aff1
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-11-21 09:12:52 +0530

    test

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -24,15 +26,50 @@ public class MyServiceServlet extends HttpServlet {
 	
 	private static final Logger log = Logger.getLogger(MyServiceServlet.class.getName());
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		log.info("inside get method");
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Filter p = new FilterPredicate("EmpID", FilterOperator.EQUAL, "18831");
-		Query q = new Query("PersistentEmployees").setFilter(p);
-		PreparedQuery pq = datastore.prepare(q);
-		Entity result = pq.asSingleEntity();
-		log.info(result.toString());
+		resp.setContentType("application/json");
+		if(req.getParameter("EmpID")!=null)
+		{
+			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+			Filter p = new FilterPredicate("EmpID", FilterOperator.EQUAL, req.getParameter("EmpID"));
+			Query q = new Query("PersistentEmployees").setFilter(p);
+			PreparedQuery pq = datastore.prepare(q);
+			Entity result = pq.asSingleEntity();
+			log.info(result.toString());
+			JSONObject resObj = new JSONObject();
+			resObj.put("EmpID", result.getProperty("EmpID"));
+			resObj.put("EmployeeName", result.getProperty("EmployeeName"));
+			resObj.put("Gender", result.getProperty("Gender"));
+			resObj.put("Extention", result.getProperty("Extention"));
+			resObj.put("DateOfBirth", result.getProperty("DateOfBirth"));
+			resObj.put("DateOfJoining", result.getProperty("DateOfJoining"));
+			resObj.put("LastLeaveDate", result.getProperty("LastLeaveDate"));
+			resObj.put("LastLeaveReason", result.getProperty("LastLeaveReason"));
+			resp.getWriter().write(resObj.toJSONString());
+		}
+		if(req.getParameter("EmployeeName")!=null)
+		{
+			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+			Filter p = new FilterPredicate("EmployeeName", FilterOperator.EQUAL, req.getParameter("EmployeeName"));
+			Query q = new Query("PersistentEmployees").setFilter(p);
+			PreparedQuery pq = datastore.prepare(q);
+			Entity result = pq.asSingleEntity();
+			log.info(result.toString());
+			JSONObject resObj = new JSONObject();
+			resObj.put("EmpID", result.getProperty("EmpID"));
+			resObj.put("EmployeeName", result.getProperty("EmployeeName"));
+			resObj.put("Gender", result.getProperty("Gender"));
+			resObj.put("Extention", result.getProperty("Extention"));
+			resObj.put("DateOfBirth", result.getProperty("DateOfBirth"));
+			resObj.put("DateOfJoining", result.getProperty("DateOfJoining"));
+			resObj.put("LastLeaveDate", result.getProperty("LastLeaveDate"));
+			resObj.put("LastLeaveReason", result.getProperty("LastLeaveReason"));
+			resp.getWriter().write(resObj.toJSONString());
+		}
+		
 	}
 }

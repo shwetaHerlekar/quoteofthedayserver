6eabd4db279733d07f5c2fe2e73d8c43c4b14e5e
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-12-06 14:12:40 +0530

    col added

--- src/main/java/com/example/LeaveTransaction.java
+++ src/main/java/com/example/LeaveTransaction.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -20,82 +20,13 @@ import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 
-
-// [START example]
-@SuppressWarnings("serial")
-public class MyServiceServlet extends HttpServlet {
-	
+/**
+ * Servlet implementation class LeaveTransaction
+ */
+public class LeaveTransaction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
 	private static final Logger log = Logger.getLogger(MyServiceServlet.class.getName());
-
-	@SuppressWarnings({ "unchecked", "unused" })
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		log.info("inside get method");
-		resp.setContentType("application/json");
-		JSONObject resObj = new JSONObject();
-		try{
-			
-			//create datastore service object
-			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-			Filter filter = null;
-			
-			//set the filter either EmpID or EmployeeName
-			if(req.getParameter("EmpID")!=null)
-			{
-				 log.info("got employee id");
-				 filter = new FilterPredicate("EmpID", FilterOperator.EQUAL, req.getParameter("EmpID"));
-			}
-			if(req.getParameter("EmployeeName")!=null)
-			{
-				log.info("got employee name");
-				filter = new FilterPredicate("EmployeeName", FilterOperator.EQUAL, req.getParameter("EmployeeName"));
-			}
-			
-			//prepare query
-			Query q = new Query("PSLEmployee").setFilter(filter);
-			PreparedQuery pq = datastore.prepare(q);
-			
-			//fetch respective entity
-			Entity result = pq.asSingleEntity();
-			log.info(result.toString());
-			
-			if(result==null){
-				resObj.put("ERROR_MSG", Constants.ERROR_MSGS[1]);
-				resObj.put("error_code", Constants.ERROR_CODES[1]);
-			}else{
-				//create json object
-				
-				resObj.put("EmpID", result.getProperty("EmpID"));
-				resObj.put("EmployeeName", result.getProperty("EmployeeName"));
-				resObj.put("BusinessUnit", result.getProperty("BusinessUnit"));
-				resObj.put("Gender", result.getProperty("Gender"));
-				resObj.put("Extention", result.getProperty("Extention"));
-				resObj.put("Mobile", result.getProperty("Mobile"));
-				resObj.put("Location", result.getProperty("Location"));
-				resObj.put("ManagerName", result.getProperty("ManagerName"));
-				resObj.put("DateOfBirth", result.getProperty("DateOfBirth"));
-				resObj.put("DateOfJoining", result.getProperty("DateOfJoining"));
-				resObj.put("LastLeaveDate", result.getProperty("LastLeaveDate"));
-				resObj.put("LastLeaveReason", result.getProperty("LastLeaveReason"));
-				resObj.put("OptionalLeave", result.getProperty("OptionalLeave"));
-				resObj.put("OptionalHoliday", result.getProperty("OptionalHoliday"));
-				resObj.put("CompensatoryOff", result.getProperty("CompensatoryOff"));
-				resObj.put("PrivilegedLeave", result.getProperty("PrivilegedLeave"));
-			}
-			
-			//return the json
-			resp.getWriter().write(resObj.toJSONString());
-			
-		}catch(Exception e)
-		{
-			log.info("exception in getting employee"+e);
-			resObj.put("ERROR_MSG", Constants.ERROR_MSGS[1]);
-			resObj.put("error_code", Constants.ERROR_CODES[1]);
-			resp.getWriter().write(resObj.toJSONString());
-		}
-		
-	}
 	
 	@SuppressWarnings({ "null", "unchecked" })
 	@Override
@@ -103,6 +34,7 @@ public class MyServiceServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		JSONObject responseObj=null;
+		log.info("inside apply leave");
 		
 		try{
 			String responseJson = ReaderUtil.readPostParameter(req);
@@ -110,8 +42,7 @@ public class MyServiceServlet extends HttpServlet {
 			JSONObject jsonResponseObject;
 			jsonResponseObject = (JSONObject) parser
 					.parse(responseJson);
-			
-			
+			log.info(jsonResponseObject.toJSONString());
 			int response = addLeaveTransaction(jsonResponseObject);
 			if(response == 1){
 				response = deductLeaves(jsonResponseObject);
@@ -136,6 +67,7 @@ public class MyServiceServlet extends HttpServlet {
 	}
 	
 	public int addLeaveTransaction(JSONObject jsonResponseObject) {
+		log.info("inside add leave transaction");
 		int response = -1;
 		try{
 			String employeeName = jsonResponseObject.get("employeeName").toString();
@@ -161,6 +93,7 @@ public class MyServiceServlet extends HttpServlet {
 	}
 	
 	public int deductLeaves(JSONObject jsonResponseObject) {
+		log.info("inside deduct leaves");
 		int response = -1;
 		try{
 			String employeeName = jsonResponseObject.get("employeeName").toString();
@@ -206,4 +139,5 @@ public class MyServiceServlet extends HttpServlet {
 		}
 		return response;
 	}
+
 }

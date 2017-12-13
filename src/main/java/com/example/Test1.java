a8a1ccdd9a8acd9ae67d92d590b5018c46892691
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-12-13 18:19:56 +0530

    testing leave type,info apis

--- src/main/java/com/example/Test1.java
+++ src/main/java/com/example/Test1.java
@@ -28,16 +28,26 @@ public class Test1 extends HttpServlet {
 		log.info("inside get test1");
 		String userName = "shweta_herlekar";
 		String accessToken = "z7l0lL8vOU9epMTw1meQMAw8tyaX";
-		JSONObject obj = getResponseFromLeaveTypeAPI(accessToken, userName);
+		JSONArray leaveTypes = getResponseFromLeaveTypeAPI(accessToken, userName);
 		
 		///leaveTypeCid need to be set based on leave type
 		int leaveTypeCid = 1;
-		JSONObject obj1 = getResponseFromLeaveInfoAPI(accessToken, userName, leaveTypeCid);
+		JSONArray LeaveInfo = getResponseFromLeaveInfoAPI(accessToken, userName, leaveTypeCid);
+		JSONObject employee = (JSONObject) LeaveInfo.get(9);
+		String employeeHRISCid = employee.get("key").toString();
+		
+		//setting parameters for leave days api
+		String fromDate = "15-Dec-2017";
+		String toDate = "17-Dec-2017";
+		boolean isHalfDaySession = false;
+		
+		int leaves = getResponseFromLeaveDaysAPI(accessToken, fromDate, toDate, leaveTypeCid, isHalfDaySession, employeeHRISCid);
+		response.getWriter().write("Leave types, info and days api tested successfully");
 	}
 	
 	
-	static JSONObject getResponseFromLeaveTypeAPI(String accessToken,String userName){
-		JSONObject responseData=null;
+	static JSONArray getResponseFromLeaveTypeAPI(String accessToken,String userName){
+		JSONArray leaveTypes = null;
 		try{
 			log.info("inside getting response of api for type");
 			String apiurl = "https://api.persistent.com:9020/hr/leavetypes/"+userName;
@@ -58,18 +68,18 @@ public class Test1 extends HttpServlet {
 			}
 			
 			JSONParser parser = new JSONParser();
-			JSONArray leaveTypes = (JSONArray) parser.parse(output.toString());
+			leaveTypes = (JSONArray) parser.parse(output.toString());
 			conn.disconnect();
 			log.info(leaveTypes.toString());
 		}catch(Exception e){
 			log.info("error accessing leave balance :"+e);
 		}
 
-		return responseData;
+		return leaveTypes;
 	}
 
-	static JSONObject getResponseFromLeaveInfoAPI(String accessToken,String userName, int leaveTypeCid){
-		JSONObject responseData=null;
+	static JSONArray getResponseFromLeaveInfoAPI(String accessToken,String userName, int leaveTypeCid){
+		JSONArray leaveInfo=null;
 		try{
 			log.info("inside getting response of api for leave balance");
 			String apiurl = "https://api.persistent.com:9020/hr/leaveinfo/"+userName+"/"+leaveTypeCid;
@@ -90,13 +100,44 @@ public class Test1 extends HttpServlet {
 			}
 			
 			JSONParser parser = new JSONParser();
-			JSONArray leaveInfo= (JSONArray) parser.parse(output.toString());
+			leaveInfo= (JSONArray) parser.parse(output.toString());
 			conn.disconnect();
 			log.info(leaveInfo.toString());
 		}catch(Exception e){
 			log.info("error accessing leave balance :"+e);
 		}
 
-		return responseData;
+		return leaveInfo;
+	}
+	
+	static int getResponseFromLeaveDaysAPI(String accessToken,String fromDate, String toDate, int leaveTypeCid, boolean isHalfDaySession, String employeeHRISCid){
+		int leaves = 0;
+		try{
+			log.info("inside getting response of api for leave balance");
+			String apiurl = "https://api.persistent.com:9020/hr/leaveinfo/"+fromDate+"/"+toDate+"/"+leaveTypeCid+"/"+isHalfDaySession+"/"+employeeHRISCid;
+			URL url = new URL(apiurl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Content-Length", "0");
+			conn.setRequestProperty("Authorization", "Bearer "+accessToken);
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-Type", "application/json");
+			
+			BufferedReader bufferedReaderObject = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));			
+			StringBuilder output = new StringBuilder();			
+			
+			String op;
+			while ((op = bufferedReaderObject.readLine()) != null) {
+				output.append(op);
+			}
+			
+			leaves = Integer.parseInt(output.toString());
+			conn.disconnect();
+			log.info("no of leaves applicable"+leaves);
+		}catch(Exception e){
+			log.info("error accessing leave balance :"+e);
+		}
+
+		return leaves;
 	}
 }

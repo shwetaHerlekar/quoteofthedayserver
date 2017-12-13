6149c3cecd2e013c8780bd17a5ec8eb09c51e206
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-12-13 14:24:15 +0530

    testing leave balance api

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -26,38 +27,86 @@ public class Test extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		log.info("inside Test get");
+		log.info("inside test get");
 		String accessToken = "60OrOwPAuvDhrGBjCy33qTuqzt1i";
-		String apiurl = "https://api.persistent.com:9020/hr/leaveattendanceself";
-		URL url = new URL(apiurl);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setDoOutput(true);
-		conn.setRequestProperty("Content-Length", "0");
-		conn.setRequestProperty("Authorization", "Bearer "+accessToken);
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-Type", "application/json");
-		
-		BufferedReader bufferedReaderObject = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));			
-		StringBuilder output = new StringBuilder();			
-		
-		String op;
-		while ((op = bufferedReaderObject.readLine()) != null) {
-			output.append(op);
-		}
-		
-		JSONParser parser = new JSONParser();
-		JSONObject responseData=null;
-		try {
-			responseData = (JSONObject) parser.parse(output.toString());
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		conn.disconnect();
-		log.info(responseData.toString());
+		JSONObject responseData = getHolidays(accessToken);
 		response.getWriter().write(responseData.toJSONString());
 	}
 
+	@SuppressWarnings({ "unused", "deprecation", "unchecked" })
+	public static JSONObject getHolidays(String accessToken) {
+		// TODO Auto-generated method stub
+		
+		log.info("inside get holidays");
+		JSONObject employeeDetails = getResponseFromAPI(accessToken);
+		
+		JSONArray leaveTypes = (JSONArray)employeeDetails.get("LeavesList");
+		
+		JSONObject leave = (JSONObject)leaveTypes.get(0);
+		int privillage_leave = Integer.parseInt(leave.get("Privileged Leave").toString());
+		
+		leave = (JSONObject)leaveTypes.get(1);
+		int ol = Integer.parseInt(leave.get("Optional Leave").toString());
+		
+		leave = (JSONObject)leaveTypes.get(2);
+		int oh = Integer.parseInt(leave.get("Optional Holiday").toString());
+		
+		leave = (JSONObject)leaveTypes.get(3);
+		int cf = Integer.parseInt(leave.get("Compensatory Off").toString());
+    	
+		JSONObject responseObject = new JSONObject();
+		
+		String event_date= employeeDetails.get("DateOfBirth").toString();  
+		responseObject.put("birthday", event_date);
+		
+		JSONObject holidays = new JSONObject();
+		event_date="25/12/2017";  
+		holidays.put(event_date, "christmas");
+		
+		event_date="31/12/2017";  
+		holidays.put(event_date, "new year eve");
+		
+		responseObject.put("holidays", holidays);
+		responseObject.put("optional_leave", oh);
+		responseObject.put("optional_holiday", ol);
+		responseObject.put("compensatiory_off", cf);
+		responseObject.put("privillage_leave", privillage_leave);
+
+		log.info(responseObject.toString());
+		return responseObject;
+	
+	}
+	
+	static JSONObject getResponseFromAPI(String accessToken){
+		JSONObject responseData=null;
+		try{
+			log.info("inside getting response of api for leave balance");
+			String apiurl = "https://api.persistent.com:9020/hr/leaveattendanceself";
+			URL url = new URL(apiurl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Content-Length", "0");
+			conn.setRequestProperty("Authorization", "Bearer "+accessToken);
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-Type", "application/json");
+			
+			BufferedReader bufferedReaderObject = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));			
+			StringBuilder output = new StringBuilder();			
+			
+			String op;
+			while ((op = bufferedReaderObject.readLine()) != null) {
+				output.append(op);
+			}
+			
+			JSONParser parser = new JSONParser();
+			responseData = (JSONObject) parser.parse(output.toString());
+			conn.disconnect();
+			log.info(responseData.toString());
+		}catch(Exception e){
+			log.info("error accessing leave balance :"+e);
+		}
+
+		return responseData;
+	}
 
 }

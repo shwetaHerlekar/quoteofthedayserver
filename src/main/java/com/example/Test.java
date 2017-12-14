a1b8a44d5141051b0e83025932e36c0a200907e6
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-12-14 11:33:17 +0530

    testing leave apply api

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -3,8 +3,10 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -15,20 +17,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 
 /**
  * Servlet implementation class Test
  */
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final Logger log = Logger.getLogger(Test.class.getName());   
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private static final Logger log = Logger.getLogger(Test.class.getName());
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		log.info("inside test get");
-		String accessToken = "z7l0lL8vOU9epMTw1meQMAw8tyaX";
+		String accessToken = "omWXb77W6HrQ78xOAJKJNTlTNEpH";
 		JSONObject responseData = getHolidays(accessToken);
 		response.getWriter().write(responseData.toJSONString());
 	}
@@ -36,38 +37,42 @@ public class Test extends HttpServlet {
 	@SuppressWarnings({ "unused", "deprecation", "unchecked" })
 	public static JSONObject getHolidays(String accessToken) {
 		// TODO Auto-generated method stub
-		
+
 		log.info("inside get holidays");
 		JSONObject employeeDetails = getResponseFromAPI(accessToken);
-		
-		JSONArray leaveTypes = (JSONArray)employeeDetails.get("LeavesList");
+
+		JSONArray leaveTypes = (JSONArray) employeeDetails.get("LeavesList");
 		log.info(leaveTypes.toString());
-		
-		JSONObject leave = (JSONObject)leaveTypes.get(0);
+
+		JSONObject leave = (JSONObject) leaveTypes.get(0);
 		log.info(leave.toString());
-		float privillage_leave = Float.parseFloat(leave.get("AvailableLeaveCount").toString());
-		
-		leave = (JSONObject)leaveTypes.get(1);
-		float ol = Float.parseFloat(leave.get("AvailableLeaveCount").toString());
-		
-		leave = (JSONObject)leaveTypes.get(2);
-		float oh = Float.parseFloat(leave.get("AvailableLeaveCount").toString());
-		
-		leave = (JSONObject)leaveTypes.get(3);
-		float cf = Float.parseFloat(leave.get("AvailableLeaveCount").toString());
-    	
+		float privillage_leave = Float.parseFloat(leave.get(
+				"AvailableLeaveCount").toString());
+
+		leave = (JSONObject) leaveTypes.get(1);
+		float ol = Float
+				.parseFloat(leave.get("AvailableLeaveCount").toString());
+
+		leave = (JSONObject) leaveTypes.get(2);
+		float oh = Float
+				.parseFloat(leave.get("AvailableLeaveCount").toString());
+
+		leave = (JSONObject) leaveTypes.get(3);
+		float cf = Float
+				.parseFloat(leave.get("AvailableLeaveCount").toString());
+
 		JSONObject responseObject = new JSONObject();
-		
-		String event_date= "22/05/1995";  
+
+		String event_date = "22/05/1995";
 		responseObject.put("birthday", event_date);
-		
+
 		JSONObject holidays = new JSONObject();
-		event_date="25/12/2017";  
+		event_date = "25/12/2017";
 		holidays.put(event_date, "christmas");
-		
-		event_date="31/12/2017";  
+
+		event_date = "31/12/2017";
 		holidays.put(event_date, "new year eve");
-		
+
 		responseObject.put("holidays", holidays);
 		responseObject.put("optional_leave", ol);
 		responseObject.put("optional_holiday", oh);
@@ -76,39 +81,90 @@ public class Test extends HttpServlet {
 
 		log.info(responseObject.toString());
 		return responseObject;
-	
+
 	}
-	
-	static JSONObject getResponseFromAPI(String accessToken){
-		JSONObject responseData=null;
-		try{
+
+	static JSONObject getResponseFromAPI(String accessToken) {
+		JSONObject responseData = null;
+		try {
 			log.info("inside getting response of api for leave balance");
 			String apiurl = "https://api.persistent.com:9020/hr/leaveattendanceself";
 			URL url = new URL(apiurl);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setDoOutput(true);
 			conn.setRequestProperty("Content-Length", "0");
-			conn.setRequestProperty("Authorization", "Bearer "+accessToken);
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Content-Type", "application/json");
+
+			BufferedReader bufferedReaderObject = new BufferedReader(
+					new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			StringBuilder output = new StringBuilder();
+
+			String op;
+			while ((op = bufferedReaderObject.readLine()) != null) {
+				output.append(op);
+			}
+
+			JSONParser parser = new JSONParser();
+			responseData = (JSONObject) parser.parse(output.toString());
+			conn.disconnect();
+			log.info(responseData.toString());
+		} catch (Exception e) {
+			log.info("error accessing leave balance :" + e);
+		}
+
+		return responseData;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static JSONObject applyLeave(String empName, int leaveTypeCid,
+			String fromDate, String toDate, boolean isHalfDaySession,
+			boolean isAfterNoon, int leaveYearCid, boolean isAdvancedLeave,
+			String approverID, String Reason, String accessToken) {
+		JSONObject responseData = null;
+		try {
+
+			JSONObject requestBody = new JSONObject();
+			requestBody.put("EmployeeUserName", empName);
+			requestBody.put("LeaveTypeCid", leaveTypeCid);
+			requestBody.put("FromDate", fromDate);
+			requestBody.put("ToDate", toDate);
+			requestBody.put("IsHalfDaySession", isHalfDaySession);
+			requestBody.put("IsAfterNoon", isAfterNoon);
+			requestBody.put("LeaveYearCid", leaveYearCid);
+			requestBody.put("IsAdvanceLeave", isAdvancedLeave);
+			requestBody.put("ApproverId", approverID);
+			requestBody.put("Reason", Reason);
+			log.info("raw post data :"+requestBody);
+			byte[] out = requestBody.toJSONString().getBytes(StandardCharsets.UTF_8);
 			
-			BufferedReader bufferedReaderObject = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));			
+			URL u = new URL("https://api.persistent.com:9020/hr/leave");
+			HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty( "Content-Type", "application/json" );
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+			conn.setRequestProperty( "Content-Length", String.valueOf(out.length));
+			OutputStream os = conn.getOutputStream();
+			os.write(out);
+			os.flush();
+			
+			BufferedReader bufferedReaderObject = new BufferedReader(new InputStreamReader((conn.getInputStream())));			
 			StringBuilder output = new StringBuilder();			
 			
 			String op;
 			while ((op = bufferedReaderObject.readLine()) != null) {
 				output.append(op);
 			}
-			
+
 			JSONParser parser = new JSONParser();
 			responseData = (JSONObject) parser.parse(output.toString());
+			log.info("resposne from apply leave API:"+responseData);
 			conn.disconnect();
-			log.info(responseData.toString());
-		}catch(Exception e){
-			log.info("error accessing leave balance :"+e);
+		} catch (Exception e) {
+			log.info("exception in applying leave" + e);
 		}
-
 		return responseData;
 	}
-
 }
--- src/main/java/com/example/Test1.java
+++ src/main/java/com/example/Test1.java
@@ -3,8 +3,10 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -27,14 +29,23 @@ public class Test1 extends HttpServlet {
 		// TODO Auto-generated method stub
 		log.info("inside get test1");
 		String userName = "shweta_herlekar";
-		String accessToken = "z7l0lL8vOU9epMTw1meQMAw8tyaX";
+		String accessToken = "omWXb77W6HrQ78xOAJKJNTlTNEpH";
 		JSONArray leaveTypes = getResponseFromLeaveTypeAPI(accessToken, userName);
+		JSONObject obj = (JSONObject) leaveTypes.get(7);
+		int leaveYearCid = Integer.parseInt(obj.get("key").toString());
+		obj = (JSONObject) leaveTypes.get(1);
+		int leaveTypeCid = Integer.parseInt(obj.get("key").toString());
 		
 		///leaveTypeCid need to be set based on leave type
-		int leaveTypeCid = 1;
+		
 		JSONArray LeaveInfo = getResponseFromLeaveInfoAPI(accessToken, userName, leaveTypeCid);
 		JSONObject employee = (JSONObject) LeaveInfo.get(9);
 		String employeeHRISCid = employee.get("key").toString();
+		String empName = employee.get("value").toString();
+		log.info("employee name :"+empName);
+		
+		JSONObject approver = (JSONObject)LeaveInfo.get(4);
+		String approverID = approver.get("key").toString();
 		
 		//setting parameters for leave days api
 		String fromDate = "15-Dec-2017";
@@ -43,6 +54,13 @@ public class Test1 extends HttpServlet {
 		
 		float leaves = getResponseFromLeaveDaysAPI(accessToken, fromDate, toDate, leaveTypeCid, isHalfDaySession, employeeHRISCid);
 		response.getWriter().write("Leave types, info and days api tested successfully");
+		
+		//setting parameters for leave apply parameters
+		boolean isAfterNoon = false;
+		boolean isAdvancedLeave = false;
+		String Reason = "testing leave apply api";
+		
+		JSONObject res = applyLeave(empName, leaveTypeCid, fromDate, toDate, isHalfDaySession, isAfterNoon, leaveYearCid, isAdvancedLeave, approverID, Reason, accessToken);
 	}
 	
 	
@@ -139,5 +157,56 @@ public class Test1 extends HttpServlet {
 		}
 
 		return leaves;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static JSONObject applyLeave(String empName, int leaveTypeCid,
+			String fromDate, String toDate, boolean isHalfDaySession,
+			boolean isAfterNoon, int leaveYearCid, boolean isAdvancedLeave,
+			String approverID, String Reason, String accessToken) {
+		JSONObject responseData = null;
+		try {
+
+			JSONObject requestBody = new JSONObject();
+			requestBody.put("EmployeeUserName", empName);
+			requestBody.put("LeaveTypeCid", leaveTypeCid);
+			requestBody.put("FromDate", fromDate);
+			requestBody.put("ToDate", toDate);
+			requestBody.put("IsHalfDaySession", isHalfDaySession);
+			requestBody.put("IsAfterNoon", isAfterNoon);
+			requestBody.put("LeaveYearCid", leaveYearCid);
+			requestBody.put("IsAdvanceLeave", isAdvancedLeave);
+			requestBody.put("ApproverId", approverID);
+			requestBody.put("Reason", Reason);
+			log.info("raw post data :"+requestBody);
+			byte[] out = requestBody.toJSONString().getBytes(StandardCharsets.UTF_8);
+			
+			URL u = new URL("https://api.persistent.com:9020/hr/leave");
+			HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty( "Content-Type", "application/json" );
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+			conn.setRequestProperty( "Content-Length", String.valueOf(out.length));
+			OutputStream os = conn.getOutputStream();
+			os.write(out);
+			os.flush();
+			
+			BufferedReader bufferedReaderObject = new BufferedReader(new InputStreamReader((conn.getInputStream())));			
+			StringBuilder output = new StringBuilder();			
+			
+			String op;
+			while ((op = bufferedReaderObject.readLine()) != null) {
+				output.append(op);
+			}
+
+			JSONParser parser = new JSONParser();
+			responseData = (JSONObject) parser.parse(output.toString());
+			log.info("resposne from apply leave API:"+responseData);
+			conn.disconnect();
+		} catch (Exception e) {
+			log.info("exception in applying leave" + e);
+		}
+		return responseData;
 	}
 }

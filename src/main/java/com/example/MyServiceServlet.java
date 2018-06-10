7a6f572bd4d295768ecf6554928caabc1c27f410
Shweta Herlekar <shweta_herlekar@persistent.com>
2018-06-10 18:06:19 +0530

    added quotes

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -27,14 +28,60 @@ public class MyServiceServlet extends HttpServlet {
 		//JSONObject resObj = new JSONObject();
 		try{
 			
-			String a = "Hello quote world!!";
-			resp.getWriter().write(a);
+			String quote = readFile("quotes.txt");
+			resp.getWriter().write(quote);
 			
 		}catch(Exception e)
 		{
 			log.info("exception in getting employee"+e);
 		}
 		
+	}
+	
+	public String readFile(String fileName){
+		StringBuilder result = new StringBuilder("");
+		String quote = new String("");
+		//Get file from resources folder
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(fileName).getFile());
+		
+		Boolean isFirstLine = true;
+		try (Scanner scanner = new Scanner(file)) {
+
+			while (scanner.hasNextLine()) {
+				if(isFirstLine){
+					quote = scanner.nextLine();
+					isFirstLine = false;
+				}
+				else{
+					String line = scanner.nextLine();
+					result.append(line).append("\n");
+				}
+			}
+
+			scanner.close();
+			result.append(quote).append("\n");
+			writeToFile(fileName, result.toString());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		return quote;
+
+	  }
+	
+	public void writeToFile(String fileName,String data){
+		log.info("dataaaaa:"+data);
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(fileName).getFile());
+		try {
+			FileWriter fr = new FileWriter(file);
+			BufferedWriter br = new BufferedWriter(fr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }

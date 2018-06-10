010e2de8da1464541d7f22daeadda065ec0dcd1f
Shweta Herlekar <shweta_herlekar@persistent.com>
2018-06-10 20:10:49 +0530

    added quotes and images

--- src/main/java/com/example/MyServiceServlet.java
+++ src/main/java/com/example/MyServiceServlet.java
@@ -26,15 +26,21 @@ public class MyServiceServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		log.info("inside get method");
-		//resp.setContentType("application/json");
+		resp.setContentType("application/json");
 		//JSONObject resObj = new JSONObject();
 		try{
 			
 			Date today = new Date();
+			String root = "https://storage.googleapis.com/data-quoteapp/back";
 			int index = today.getDate();
+			int[] imagecnts = new int[]{1,2};
 			log.info("index :"+index);
+			int imgcnt = index % 2;
+			JSONObject obj = new JSONObject();
 			String quote = readQuote("quotes.txt",index);
-			resp.getWriter().write(quote);
+			obj.put("quote", quote);
+			obj.put("image_url", root+imgcnt+".jpg");
+			resp.getWriter().write(obj.toJSONString());
 			
 		}catch(Exception e)
 		{
@@ -65,7 +71,7 @@ public class MyServiceServlet extends HttpServlet {
 			log.info("exception in getting quotes"+e);
 		}
 			
-		return quotes.get(index);
+		return quotes.get(index-1);
 	}
 	
 }
